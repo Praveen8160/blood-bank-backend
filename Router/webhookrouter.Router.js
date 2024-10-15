204cc9af52311c5be68337bcf49d5ef022c09982
Praveen8160 <praveenp8160@gmail.com>
2024-10-15 18:19:20 +0530

    Update FindDonorByBloodType intent

--- Router/webhookrouter.Router.js
+++ Router/webhookrouter.Router.js
@@ -3,35 +3,38 @@ const Router = express.Router();
 const Donor = require("../models/Donor.model.js");
 Router.post("/", async (req, res) => {
   const intentName = req.body.queryResult.intent.displayName;
-  console.log("body", req.body);
-  console.log("req.body.queryResult", req.body.queryResult);
-  console.log("req.body.queryResult.intent", req.body.queryResult.intent);
-  console.log("intentName", intentName);
-  if (intentName === "FindDonorByBloodType") {
-    const bloodType = req.body.queryResult.parameters.bloodType;
-    console.log("bloodType", bloodType);
-    // Query the database for matching donors
-    const donors = await Donor.find({ bloodGroup: bloodType }).select(
-      "fullname address state district pincode mobile"
-    );
-    if (donors.length > 0) {
-      let donorList = donors
-        .map((donor) => {
-          return `Name: ${donor.fullname},Address: ${donor.address}${donor.state}${donor.district}${donor.pincode}, Mobile: ${donor.mobile} || `;
-        })
-        .join(",");
-      res.json({
-        fulfillmentText: `Here are the donors with blood type ${bloodType}:${"\n"}${donorList}`,
-      });
-    } else {
-      res.json({
-        fulfillmentText: `Sorry, we couldn't find any donors with blood type ${bloodType}.`,
-      });
-    }
-  } else {
-    res.json({
-      fulfillmentText: "I didn't understand your request.",
-    });
+  // console.log("body", req.body);
+  // console.log("req.body.queryResult", req.body.queryResult);
+  // console.log("req.body.queryResult.intent", req.body.queryResult.intent);
+  // console.log("intentName", intentName);
+  switch (intentName) {
+    case "FindDonorByBloodType":
+      const bloodType = req.body.queryResult.parameters.bloodType;
+      const State = req.body.queryResult.parameters.State;
+      const District = req.body.queryResult.parameters.District;
+      // console.log("bloodType", bloodType);
+      // Query the database for matching donors
+      const donors = await Donor.find({
+        bloodGroup: bloodType,
+        state: State,
+        district: District,
+      }).select("fullname address state district pincode mobile");
+      if (donors.length > 0) {
+        let donorList = donors
+          .map((donor) => {
+            return `Name: ${donor.fullname},Address: ${donor.address}${donor.state}${donor.district}${donor.pincode}, Mobile: ${donor.mobile} || `;
+          })
+          .join(",");
+        res.json({
+          fulfillmentText: `Here are the donors with blood type ${bloodType}:${"\n"}${donorList}`,
+        });
+      } else {
+        res.json({
+          fulfillmentText: `Sorry, we couldn't find any donors with blood type ${bloodType}.`,
+        });
+      }
+      break;
+    case "FindDonorByLocation":
   }
 });
 module.exports = Router;
